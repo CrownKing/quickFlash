5fcda2dd0e0b8c68ed5a76962700f8083987b7d0
Jonathan <jonathan.lopes@mmtools.com.br>
2024-01-15 22:27:54 -0300

    correções na responsividade do app

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -4,18 +4,25 @@ import {FontAwesomeIcon} from'@fortawesome/react-fontawesome'
 import {faCircleUser} from '@fortawesome/free-solid-svg-icons'
 import {faCoins} from '@fortawesome/free-solid-svg-icons'
 import cards from '../icons/cards.png';
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <div className='navBackGround'>
         <div>
+        <Link to="/perfil">
         <FontAwesomeIcon icon = {faCircleUser} size='2x'></FontAwesomeIcon>
+        </Link>
         </div>
         <div>
+        <Link to="/home">
             <img src={cards} alt="Logo" className='card' />
+        </Link>
         </div>
         <div>
+        <Link to="/points">
         <FontAwesomeIcon icon = {faCoins} size='2x'></FontAwesomeIcon>
+        </Link>
         </div>
     </div>
   )
